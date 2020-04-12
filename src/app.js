df3b09d319694abb52a117d66e79be875c18262c
BrunoSaibert <brunosaibert@gmail.com>
2020-04-12 10:44:30 -0300

    Adicionar middleware para validar o ID

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,6 +9,16 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+function validateId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid ID." });
+  }
+
+  return next();
+}
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
@@ -24,7 +34,7 @@ app.post("/repositories", (request, response) => {
   return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.put("/repositories/:id", validateId, (request, response) => {
   const { id } = request.params;
   const { title, url, techs } = request.body;
 
@@ -49,7 +59,7 @@ app.put("/repositories/:id", (request, response) => {
   return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
+app.delete("/repositories/:id", validateId, (request, response) => {
   const { id } = request.params;
 
   const repositoryIndex = repositories.findIndex(
@@ -65,7 +75,7 @@ app.delete("/repositories/:id", (request, response) => {
   return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like", validateId, (request, response) => {
   const { id } = request.params;
 
   const findRepository = repositories.find(
